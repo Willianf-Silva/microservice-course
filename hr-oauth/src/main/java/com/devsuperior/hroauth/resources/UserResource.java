2b905dffec19680e71e5363c56f54f4a019268dd
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2021-06-26 10:20:59 -0300

    UserFeignClient

--- hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
@@ -1,15 +1,14 @@
-package com.devsuperior.hruser.resources;
+package com.devsuperior.hroauth.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.hruser.entities.User;
-import com.devsuperior.hruser.services.UserService;
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.services.UserService;
 
 @RequestMapping("/users")
 @RestController
@@ -18,15 +17,9 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User response = userService.findById(id);
-		return ResponseEntity.ok(response );
-	}
-	
 	@GetMapping("/search")
 	public ResponseEntity<User> findByEmail(@RequestParam String email){
 		User response = userService.findByEmail(email);
-		return ResponseEntity.ok(response );
+		return ResponseEntity.ok(response);
 	}
 }
